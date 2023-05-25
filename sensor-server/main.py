0de5b55bb522490b1159e96edf1eb06980ff479d
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-05-25 20:02:37 +0900

    feat: ✨ 単純な顔認証機能を作成した。

--- sensor-server/main.py
+++ sensor-server/main.py
@@ -1,52 +1,118 @@
 import face_recognition
+import os
 import cv2
+import numpy as np
+import time
 
-# This is a demo of blurring faces in video.
+# assets/[personId]/[imageId].(png|jpg)
 
-# PLEASE NOTE: This example requires OpenCV (the `cv2` library) to be installed only to read from your webcam.
-# OpenCV is *not* required to use the face_recognition library. It's only required if you want to run this
-# specific demo. If you have trouble installing it, try any of the other demos that don't require it instead.
 
-# Get a reference to webcam #0 (the default one)
-video_capture = cv2.VideoCapture(0)
+# 顔識別器を訓練する
+def train_face_recognizer(root_dir):
+    known_face_encodings = []
+    known_face_ids = []
 
-# Initialize some variables
-face_locations = []
+    for person_id in os.listdir(root_dir):
+        person_dir = os.path.join(root_dir, person_id)
+
+        if os.path.isdir(person_dir):
+            for image_name in os.listdir(person_dir):
+                image_path = os.path.join(person_dir, image_name)
+                image = face_recognition.load_image_file(image_path)
+                face_encodings = face_recognition.face_encodings(image)
+
+                if len(face_encodings) > 0:
+                    known_face_encodings.append(face_encodings[0])
+                    known_face_ids.append(person_id)
+
+    return known_face_encodings, known_face_ids
+
+
+# 与えられた判断材料の顔写真から、顔識別器を訓練する
+known_face_encodings, known_face_ids = train_face_recognizer("assets")
+
+# FPS計算用の変数
+fps = 0
+frame_counter = 0
+start_time = time.time()
+
+# カメラからの映像を取得する
+# カメラの設定を行います。0は通常、システムのデフォルトのカメラを指します。
+cap = cv2.VideoCapture(0)
 
 while True:
-    # Grab a single frame of video
-    ret, frame = video_capture.read()
-
-    # Resize frame of video to 1/4 size for faster face detection processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
-
-    # Find all the faces and face encodings in the current frame of video
-    face_locations = face_recognition.face_locations(small_frame, model="cnn")
-
-    # Display the results
-    for top, right, bottom, left in face_locations:
-        # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
-
-        # Extract the region of the image that contains the face
-        face_image = frame[top:bottom, left:right]
-
-        # Blur the face image
-        face_image = cv2.GaussianBlur(face_image, (99, 99), 30)
-
-        # Put the blurred face region back into the frame image
-        frame[top:bottom, left:right] = face_image
-
-    # Display the resulting image
-    cv2.imshow('Video', frame)
-
-    # Hit 'q' on the keyboard to quit!
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # カメラからフレームを読み込みます。retはフレームが正しく読み込まれたかどうかを示すブール値で、
+    # frameは読み込まれたフレーム（numpy配列）です。
+    ret, frame = cap.read()
+    if not ret:
         break
 
-# Release handle to the webcam
-video_capture.release()
+    # 顔を検出する
+    # OpenCVはBGRで画像を読み込むので、face_recognitionが期待するRGB形式に変換します。
+    # Refer: https://github.com/ageitgey/face_recognition/issues/1497#issuecomment-1529567951
+    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # 顔の位置を検出します。
+    face_locations = face_recognition.face_locations(rgb_frame)
+    if len(face_locations) == 0:
+        print("No faces were detected in the frame.")
+    else:
+        print(f"Detected {len(face_locations)} face(s) in the frame.")
+        # Print face locations
+        for top, right, bottom, left in face_locations:
+            print(f"({top}, {right}, {bottom}, {left})")
+
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+
+    for (top, right, bottom, left), face_encoding in zip(
+        face_locations, face_encodings
+    ):
+        # 顔が誰のものであるかを判定する
+        matches = face_recognition.compare_faces(
+            known_face_encodings, face_encoding, 0.8
+        )
+
+        if True in matches:
+            first_match_index = matches.index(True)
+            person_id = known_face_ids[first_match_index]
+        else:
+            person_id = "Unknown"
+
+        # 顔の領域とそのIDを表示する
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.putText(
+            frame,
+            person_id,
+            (left + 6, bottom - 6),
+            cv2.FONT_HERSHEY_DUPLEX,
+            1.0,
+            (255, 255, 255),
+            1,
+        )
+
+    # FPSを計算して表示する
+    frame_counter += 1
+    elapsed_time = time.time() - start_time
+    if elapsed_time >= 1:
+        fps = frame_counter / elapsed_time
+        frame_counter = 0
+        start_time = time.time()
+    cv2.putText(
+        frame,
+        f"FPS: {fps:.2f}",
+        (10, 30),
+        cv2.FONT_HERSHEY_DUPLEX,
+        1.0,
+        (0, 255, 0),
+        1,
+    )
+
+    # 最終的な画像を表示する
+    cv2.imshow("Video", frame)
+
+    # キーボードの'q'キーまたは'esc'キーを押すと終了する
+    if cv2.waitKey(1) & 0xFF in [27, ord("q")]:
+        break
+
+# Webカメラへのアクセスを解放する
+cap.release()
 cv2.destroyAllWindows()
