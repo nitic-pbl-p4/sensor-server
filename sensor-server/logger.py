9ecb8679729cb0aebe58d2dc5bbe593b708261f2
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-06-09 15:49:01 +0900

    feat: ✨ (logger) Richロガーの設定を追加した。

---
+++ sensor-server/logger.py
@@ -0,0 +1,19 @@
+from rich.logging import RichHandler
+import logging
+from rich.console import Console
+from rich.progress import track
+from rich.highlighter import ReprHighlighter
+
+# Richを用いたロガー用の設定
+# Refer: https://rich.readthedocs.io/en/latest/logging.html
+FORMAT = "%(levelname)s %(asctime)s %(message)s %(pathname)s:%(lineno)d"
+logging.basicConfig(
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+logging.debug("Rich をロガーとして設定しました")
+
+# Richを用いたコンソール出力用の設定
+# Refer: https://github.com/Textualize/rich#using-the-console
+console = Console()
+highlighter = ReprHighlighter()
