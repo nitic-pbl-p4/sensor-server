8ba8a1bdead0a0e1184ab63b2d3228074b253db0
Reo Hakuta <reonaldhkt+webs@gmail.com>
2023-06-16 16:48:41 +0900

    feat: ✨ (state.py) 状態を表すためのクラス`State`と、本と借りる人を表す`Person` `Book`クラスを追加した。

---
+++ sensor-server/state.py
@@ -0,0 +1,79 @@
+from typing import Optional, List
+from datetime import datetime
+from queue import Queue
+from logger import *
+
+
+class Person:
+    def __init__(self, id: str, timestamp: datetime) -> None:
+        if len(id) < 1:
+            raise ValueError("Invalid id for person")
+        self.id = id
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return f"Person(id={self.id}, timestamp={self.timestamp})"
+
+
+class Book:
+    def __init__(self, id: str, timestamp: datetime) -> None:
+        if len(id) < 1:
+            raise ValueError("Invalid id for book")
+        self.id = id
+        self.timestamp = timestamp
+
+    def __repr__(self):
+        return f"Book(id={self.id}, timestamp={self.timestamp})"
+
+
+class State:
+    def __init__(self) -> None:
+        self._person: Optional[Person] = None
+        self._book_queue: Queue[Book] = Queue(maxsize=20)
+
+    @property
+    def person(self) -> Optional[Person]:
+        return self._person
+
+    @person.setter
+    def person(self, value: Optional[Person]) -> None:
+        if value and not isinstance(value, Person):
+            raise ValueError("Invalid value for person")
+        self._person = value
+
+    # getterを追加
+    def get_person(self) -> Optional[Person]:
+        return self._person
+
+    def add_book(self, book: Book) -> None:
+        # bookがBookのインスタンスか確かめる
+        if not isinstance(book, Book):
+            raise ValueError("Invalid value for book")
+        # book_queueにbookを追加する
+        # 挿入できる余裕がない場合は、一つ削除する
+        if self._book_queue.full():
+            self._book_queue.get(block=False)
+
+        self._book_queue.put(book, block=False)
+
+        return
+
+    def get_books(self) -> List[Book]:
+        return list(self._book_queue.queue)
+
+    def __repr__(self):
+        return (
+            f"State(person={self._person}, book_queue={list(self._book_queue.queue)})"
+        )
+
+
+if __name__ == "__main__":
+    my_obj = State()
+
+    my_obj.person = Person(id="123", timestamp=datetime.now())
+    console.log(f"my_obj.get_person(): {highlighter(repr(my_obj.get_person()))}")
+
+    for i in range(50):
+        my_obj.add_book(Book(id=f"book{i}", timestamp=datetime.now()))
+
+    console.log(f"my_obj.get_books(): {highlighter(repr(my_obj.get_books()))}")
